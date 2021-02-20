0a8d8a7a1432966854c726a7d7c710abe9af3cc1
Dobi Tamas <dobi.tamas0428@gmail.com>
2021-02-20 19:29:20 +0100

    Created basic entities for the application, haven't ran migration script

---
+++ MechanicHelper/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MechanicHelper;
+using MechanicHelper.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MechanicHelper.Areas.Identity.IdentityHostingStartup))]
+namespace MechanicHelper.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AccountDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AccountDbContextConnection")));
+
+                services.AddDefaultIdentity<MechanicUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AccountDbContext>();
+            });
+        }
+    }
+}
---
+++ MechanicHelper/Models/RepairService.cs
@@ -0,0 +1,35 @@
+﻿using MechanicHelper.Areas.Identity.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MechanicHelper.Models
+{
+    public class RepairService
+    {
+        [Key]
+        private int RepairServiceId { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(255)")]
+        private string Title { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(255)")]
+        private string Description { get; set; }
+
+        [Required]
+        private int Price { get; set; }
+
+        [Required]
+        private bool IsDone { get; set; } = false;
+
+        [Required]
+        private int ServicedById { get; set; }
+
+        private MechanicUser ServicedBy { get; set; }
+    }
+}
